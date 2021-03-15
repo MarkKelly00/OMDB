96c978fd14e3bb53a38822ba1f91860d48aa411a
Mako3 <big.mako@gmail.com>
2021-03-14 22:44:34 -0700

    using useState hook to store nominated movies

--- src/components/MovieDetail.js
+++ src/components/MovieDetail.js
@@ -1,16 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import MovieContext from "../utils/movieContext";
 
 function MovieDetail() {
+  const [title, setTitle] = useState('');
+  console.log(title);
+
   return (
-    <MovieContext.Consumer>
+    <MovieContext.Consumer title={title}>
       {({result: {Title, Poster, Director, Genre, Released}}) => (
         <div className="text-center">
           <img alt={Title} className="img-fluid" src={Poster} style={{ margin: "0 auto" }} />
           <h3>Director(s): {Director}</h3>
           <h3>Genre: {Genre}</h3>
           <h3>Released: {Released}</h3>
-          <button className="btn btn-success">Nominate</button>
+          <button className="btn btn-success" onClick={() => setTitle({Title})}>Nominate</button>
         </div>
       )}
     </MovieContext.Consumer>
--- src/components/Nomination.js
+++ src/components/Nomination.js
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import MovieContext from "../utils/movieContext";
+import MovieDetail from '../components/MovieDetail';
 
-function NominationList() {
+function NominationList(props) {
+  console.log('props are ', props)
+
   return (
-        <MovieContext.Consumer>
+        <MovieContext.Consumer >
       {({result: {Title}}) => (
         <div className="text-center">
           <button className="btn btn-danger" style={{ height: "30px", width: "20px", margin: "0 auto", float: "right"}}>X</button>
-          <p style={{ margin: "0 auto", fontSize: '20px'}}>{Title}</p>
+          <p style={{ margin: "0 auto", fontSize: '20px'}}>{props.title}</p>
         </div>
       )}
     </MovieContext.Consumer>
